c2b6e841f4ad7215e1c5752abdfc98fc48631313
Hassan Abdur Rauf <hassan_abdur_rauf@epam.com>
2019-11-25 16:53:06 +0100

    Day end

--- src/test/java/com/hassan/TransactionProcessingSystem/TransactionProcessingSystemApplicationTests.java
+++ src/test/java/com/hassan/TransactionProcessingSystem/TransactionProcessingSystemApplicationTests.java
@@ -4,25 +4,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.hassan.TransactionProcessingSystem.bean.Account;
 import com.hassan.TransactionProcessingSystem.bean.TransRequest;
 import com.hassan.TransactionProcessingSystem.bean.TransactionLog;
 import com.hassan.TransactionProcessingSystem.repository.AccountsRepository;
 import com.hassan.TransactionProcessingSystem.repository.TransRequestsRepository;
-import com.hassan.TransactionProcessingSystem.service.TransactionLoggingService;
 import com.hassan.TransactionProcessingSystem.service.TransactionService;
 import com.hassan.TransactionProcessingSystem.util.Constants;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles({"test"})
 class TransactionProcessingSystemApplicationTests {
 	
 	private static int ACCOUNT_FROM_ID = 1;
@@ -35,15 +41,13 @@ class TransactionProcessingSystemApplicationTests {
 	AccountsRepository accountsRepository;
 	@Mock
 	TransRequestsRepository transRequestsRepository;
-	@Mock
-	TransactionLoggingService transactionLoggingService;
 	
 	Account accountfrom;
 	Account accountTo;
 	
 	TransRequest transRequest;
 	
-	@Mock
+	@InjectMocks
 	TransactionService transactionService;
 	
 	@BeforeTestMethod
@@ -51,7 +55,12 @@ class TransactionProcessingSystemApplicationTests {
 		MockitoAnnotations.initMocks(this);
 		Mockito.when(accountsRepository.getOne(ACCOUNT_FROM_ID)).thenReturn(accountfrom);
 		Mockito.when(accountsRepository.getOne(ACCOUNT_TO_ID)).thenReturn(accountTo);
-		
+	}
+	
+	
+	
+	@BeforeEach
+	public void initialize() {
 		accountfrom = new Account();
 		accountfrom.setAccountID(ACCOUNT_FROM_ID);
 		accountfrom.setBalance(100.0);
@@ -72,7 +81,6 @@ class TransactionProcessingSystemApplicationTests {
 	void testTransferMoneySameCurrencyWithSufficientFunds() throws IOException {
 		Mockito.when(accountsRepository.getOne(ACCOUNT_FROM_ID)).thenReturn(accountfrom);
 		Mockito.when(accountsRepository.getOne(ACCOUNT_TO_ID)).thenReturn(accountTo);
-		Mockito.when(transactionLoggingService.logTransaction(new TransactionLog())).thenReturn(true);
 		assertEquals(Constants.RESPONSE_CODE_SUCCESSFULL,transactionService.processtransaction(transRequest).getResponseCode());
 	}
 	
